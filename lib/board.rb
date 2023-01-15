3c71494016fc20d3b009fd1cacc693ba9106de5a
Kurosh Hesamshariati <kuroshhesamshariati@gmail.com>
2023-01-15 10:10:42 +0330

    define check_row method in Board class

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 # instances of board will hold the game board itself and any method logic related to it
-
 class Board
+  # takes a 4x4 array to check for row wins
+  def check_rows; end
 end
