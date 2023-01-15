ead40bc4ae3cd255d38e08b05d6da27632bd3b14
Kurosh Hesamshariati <kuroshhesamshariati@gmail.com>
2023-01-15 10:25:37 +0330

    created failing test for win_rows?
    also edited the require_relative in board_spec to fix bug

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
-require_relative './lib/board'
+require_relative '../lib/board'
 
 describe Board do
+  describe '#win_rows?' do
+    subject(:my_board) { described_class.new }
+    context 'when there is no 4 in row' do
+      it 'returns false' do
+        result = my_board.win_rows?
+        expect(result).to be_win_rows
+      end
+    end
+  end
 end
